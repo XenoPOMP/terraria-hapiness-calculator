09f60e9d7ff8e16c29e8395d367c6e34ab05f95c
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-08-27 14:34:41 +0300

    change app name

--- app/app.constants.ts
+++ app/app.constants.ts
@@ -1,6 +1,6 @@
 import IAppConstants from '@/src/interfaces/AppConstants.interface';
 
 export const AppConstants: IAppConstants = {
-  appName: 'Next Template',
+  appName: 'Таблица счастья НИПов Terraria',
   maxContainerWidth: '1680px',
 };
